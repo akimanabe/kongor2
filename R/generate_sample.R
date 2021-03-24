a0e5a353eb48391b180745501322f83ebf4e7ac3
Akihiro Manabe <amanabe@affrc.go.jp>
2021-03-24 19:26:31 +0900

    :sparkles: Functionalize generate_sample

--- R/generate_sample.R
+++ R/generate_sample.R
@@ -1,22 +1,17 @@
-# generate_sample <- function(n, ages, p = c(300, 0.6, 0)) {
-# }
+generate_sample <- function(n, ages, p = c(300, 0.2, 0)) {
+  dat <- tibble::tibble(age = c(1:ages)) %>%
+    dplyr::mutate(Lmean = p[1] * (1 - exp(-p[2] * (age - p[3])))) %>%
+    dplyr::mutate(Number = 100 * exp(-M * age),
+                  Number = round(Number)) %>%
+    dplyr::mutate(Ratio = Number / sum(Number)) %>%
+    dplyr::mutate(Length =
+                    purrr::map2(.x = Lmean, .y = Ratio,
+                                function(Lmean, Ratio){
+                                  rnorm(n * Ratio,
+                                        mean = Lmean,
+                                        sd = Lmean * 0.15)}))
 
-  ages <- 7
-  n <- 500
-  p = c(300, 0.6, 0)
+  Lengths <- dat$Length %>% unlist()
 
-  p[1] * (1 - exp(-p[2]*(ages - p[3])))
-
-  L_t <- c(NA)
-
-  for(i in 1:ages+1){
-    L_t[i] <- p[1] * (1 - exp(-p[2]*(i - p[3])))
-  }
-
-  M <- 0.4
-  N_t <- c(NA)
-  for(i in 0:ages){
-    if(i == 0){N_t[i] <- n}
-  N_t[i] <- N_t[i-1] * exp(-M*i)
-  }
-
+  return(Lengths)
+}
