8e0fed6c99a0fd5ebf14820069b9e2fa9b700455
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-05 10:23:31 +0900

    :rotating_light: Remove lint warnings

--- R/fit_em_range.R
+++ R/fit_em_range.R
@@ -1,7 +1,7 @@
 #' fit_em for range of numbers of modes
 #'
 #' @param dat length data to be fitted
-#' @param Ages vector of numbers of modes
+#' @param ages vector of numbers of modes
 #'
 #' @return tibble with nested mclust result
 #' @export
@@ -11,8 +11,8 @@
 #'sampledata <- generate_sample(1000, 5)
 #' fit_em_range(sampledata, 4)
 #' fit_em_range(sampledata, c(1:10))}
-fit_em_range <- function(dat, Ages){
+fit_em_range <- function(dat, ages) {
   suppressWarnings(require(mclust))
-  tibble::tibble(Ages =  Ages) %>%
+  tibble::tibble(Ages =  ages) %>%
     dplyr::mutate(Result = purrr::map(Ages, function(x) fit_em(dat, x)))
 }
