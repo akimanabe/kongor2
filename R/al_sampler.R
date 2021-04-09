98f0db0032912ac331aa82c0ed3794dabe0e2f2e
Akihiro Manabe <amanabe@affrc.go.jp>
2021-04-09 10:33:48 +0900

    :sparkles: Add al_sampler

---
+++ R/al_sampler.R
@@ -0,0 +1,34 @@
+#' Generate Age Length sample
+#'
+#' @param n number of sample
+#' @param ages age vector
+#' @param ps vb parameter
+#' @param lambda proportion of each age
+#' @param sdlevel strength of sd, smaller the greater
+#'
+#' @return tibble
+#' @export
+al_sample <-
+  function(n, ages, ps, lambda, sdlevel = 10) {
+    means <-
+      fishgr::vb(age = ages,
+                        ps = ps) %>%
+      round()
+
+    sds <- abs(means / sdlevel)
+
+    tibble::tibble(ages, means, sds, lambda) %>%
+      dplyr::mutate(
+        Length = purrr::map2(means, sds,
+                             function(x, y) rnorm(n, mean = x, sd = y)),
+        lambda = lambda / sum(lambda),
+        lambda_N = ceiling(n * lambda),
+        Length_N = purrr::map2(Length, lambda_N,
+                               function(x, y) sample(x, y, replace = F))
+      ) %>%
+      dplyr::select(ages, means, sds, lambda, Length_N) %>%
+      tidyr::unnest(cols = c(Length_N)) %>%
+      dplyr::mutate(ages = factor(ages)) %>%
+      dplyr::rename(Length = Length_N) %>%
+      dplyr::sample_n(N)
+    }
